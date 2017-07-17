16e1437949754ebb3f034ef10c577cc4d40c92b3
takumon <inouetakumon@gmail.com>
2017-07-18 02:40:30 +0900

    サーバ側実行資産を追加

---
+++ server/routes/message.ts
@@ -0,0 +1,39 @@
+import * as http from 'http';
+import { Router, Response } from 'express';
+import { Message } from '../models/message';
+
+const messageRouter: Router = Router();
+
+// 全てのメッセージを取得する
+messageRouter.get('/', (req, res, next) => {
+  Message.find(function(err, doc) {
+    if (err) {
+      return res.send('エラーが発生しました');
+    }
+
+    return res.status(200).json({messages: doc});
+  });
+});
+
+// メッセージを登録する
+messageRouter.post('/', (req, res, next) => {
+  const message = new Message({
+    message: req.body.message
+  });
+
+  message.save((err, result) => {
+    if (err) {
+      return res.status(500).json({
+          title: 'エラーが発生しました。',
+          error: err
+      });
+    }
+
+    return res.status(200).json({
+      message: 'Saved message',
+      obj: result
+    });
+  });
+});
+
+export { messageRouter };
